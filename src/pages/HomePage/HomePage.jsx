1b8b55193c67107aaf597e266e3b7fdb634fbc4d
NinaKulyk <nina1998lukashova@gmail.com>
2024-08-18 15:31:33 +0100

    imgFix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import s from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div className={s.container}>
-      <img src="../../../public/img/sdfgh.png" alt="phonebook" width={600} />
+      <img src="/img/sdfgh.png" alt="phonebook" width={600} />
       <h2>
         ALL YOUR CONTACTS <br /> IN ONE PLACE
       </h2>
